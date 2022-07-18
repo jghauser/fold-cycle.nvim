d3115901fa47a78f573c57d4dcdb250c869afe76
jghauser <julian@julianhauser.com>
2022-07-18 15:30:35 +0200

    Fix soft_wrap_movement_fix when start within fold

--- lua/fold-cycle/utils.lua
+++ lua/fold-cycle/utils.lua
@@ -12,12 +12,25 @@ M.softwrap_movement_fix = function(movement)
   if vim.opt.wrap:get() then
     if movement == 'k' then
       local current_line = fn.line('.')
+      local folded_current = fn.foldclosed(current_line)
       local prev_line = current_line - 1
-      local folded = fn.foldclosed(prev_line)
-      if folded == -1 then
-        cmd('normal! g' .. movement)
+      local folded_prev = fn.foldclosed(prev_line)
+      if folded_current ~= (-1 or current_line) then
+        prev_line = folded_current - 1
+        folded_prev = fn.foldclosed(prev_line)
+        if folded_prev == -1 then
+          cmd('normal! ' .. folded_current .. 'G')
+          cmd('normal! g' .. movement)
+        else
+          cmd('normal! ' .. folded_current .. 'G')
+          cmd('normal! ' .. folded_prev .. 'G')
+        end
       else
-        cmd('normal! ' .. folded .. 'G')
+        if folded_prev == -1 then
+          cmd('normal! g' .. movement)
+        else
+          cmd('normal! ' .. folded_prev .. 'G')
+        end
       end
     else
       cmd('normal! g' .. movement)
