1c136cc5ce839a4090af95426d9076494675c84f
jghauser <julian@julianhauser.com>
2022-08-17 18:06:15 +0200

    Fix: softwrap movements also in visual mode

--- lua/fold-cycle/init.lua
+++ lua/fold-cycle/init.lua
@@ -17,12 +17,12 @@ M.setup = function(opts)
   if config['softwrap_movement_fix'] == true then
     M['softwrap_movement_fix'] = utils.softwrap_movement_fix
 
-    keymap.set('n', 'j',
+    keymap.set({'n', 'v'}, 'j',
       function()
         return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('j')<cr>]] or 'j'
       end,
       {silent = true, noremap = true, expr = true})
-    keymap.set('n', 'k',
+    keymap.set({'n', 'v'}, 'k',
       function()
         return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('k')<cr>]] or 'k'
       end,
