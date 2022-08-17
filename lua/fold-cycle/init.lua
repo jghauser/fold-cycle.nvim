7100300f3a8ae5b7222c07aa805f591ebbce447d
jghauser <julian@julianhauser.com>
2022-08-17 18:07:31 +0200

    Format with stylua

--- lua/fold-cycle/init.lua
+++ lua/fold-cycle/init.lua
@@ -11,28 +11,24 @@ local M = {}
 
 -- set config
 M.setup = function(opts)
-  config.update(opts)
+	config.update(opts)
 
-  -- add fix for softwrap movement over folds
-  if config['softwrap_movement_fix'] == true then
-    M['softwrap_movement_fix'] = utils.softwrap_movement_fix
+	-- add fix for softwrap movement over folds
+	if config["softwrap_movement_fix"] == true then
+		M["softwrap_movement_fix"] = utils.softwrap_movement_fix
 
-    keymap.set({'n', 'v'}, 'j',
-      function()
-        return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('j')<cr>]] or 'j'
-      end,
-      {silent = true, noremap = true, expr = true})
-    keymap.set({'n', 'v'}, 'k',
-      function()
-        return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('k')<cr>]] or 'k'
-      end,
-      {silent = true, noremap = true, expr = true})
-  end
+		keymap.set({ "n", "v" }, "j", function()
+			return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('j')<cr>]] or "j"
+		end, { silent = true, noremap = true, expr = true })
+		keymap.set({ "n", "v" }, "k", function()
+			return vim.v.count == 0 and [[<cmd>lua require('fold-cycle').softwrap_movement_fix('k')<cr>]] or "k"
+		end, { silent = true, noremap = true, expr = true })
+	end
 end
 
 -- get all functions that we need to run the various commands
 for name, command in pairs(fold_cycle) do
-  M[name] = command
+	M[name] = command
 end
 
 return M
