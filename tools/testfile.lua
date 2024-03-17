a2573c5dbdf4efee73688a2001675ae0e4e697c3
jghauser <julianhauser.com>
2024-03-17 16:47:54 +0100

    add testfile.lua for testing

---
+++ tools/testfile.lua
@@ -0,0 +1,19 @@
+local function is_fold_closed(line)
+  local folded
+  if fn.foldclosed(line) == -1 then
+    folded = false
+  else
+    folded = true
+  end
+  return folded
+end
+
+local function is_fold_closed(line)
+  local folded
+  if fn.foldclosed(line) == -1 then
+    folded = false
+  else
+    folded = true
+  end
+  return folded
+end
