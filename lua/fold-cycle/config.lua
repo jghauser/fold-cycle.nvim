6dc8052a22e396adff36567e2a1f2dc19ee289c5
jghauser <julianhauser.com>
2024-03-17 16:50:29 +0100

    format with spaces instead of tabs

--- lua/fold-cycle/config.lua
+++ lua/fold-cycle/config.lua
@@ -4,19 +4,19 @@
 
 -- default configulation values
 local default_config = {
-	open_if_max_closed = true,
-	close_if_max_opened = true,
-	softwrap_movement_fix = false,
+  open_if_max_closed = true,
+  close_if_max_opened = true,
+  softwrap_movement_fix = false,
 }
 
 local M = vim.deepcopy(default_config)
 
 -- configuration update function
 M.update = function(opts)
-	local newconf = vim.tbl_deep_extend("force", default_config, opts or {})
-	for k, v in pairs(newconf) do
-		M[k] = v
-	end
+  local newconf = vim.tbl_deep_extend("force", default_config, opts or {})
+  for k, v in pairs(newconf) do
+    M[k] = v
+  end
 end
 
 return M
